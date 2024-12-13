426426d172280b827d922642994a982fbf45ae5e
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-13 20:18:45 +0600

    Made restore section responsive

--- src/components/RestoreSection.jsx
+++ src/components/RestoreSection.jsx
@@ -4,8 +4,8 @@ import React from "react";
 const RestoreSection = () => {
   return (
     <div className="container mx-auto my-10">
-      <div className="flex gap-8">
-        <div className="bg-[#efefef] w-2/3 shadow-md rounded-xl p-4">
+      <div className="flex lg:flex-row flex-col lg:gap-8">
+        <div className="bg-[#efefef] lg:w-2/3 shadow-md rounded-xl p-4">
           <h2 className="text-[34px] text-[#67645E] font-medium mb-4">
             TO RESTORE and NURTURE the skin you have now for lasting results
             that reveal themselves over time.
@@ -13,7 +13,7 @@ const RestoreSection = () => {
           <button className="text-lg text-[#67645E] border-[#67645E] rounded-3xl border-2 font-medium px-6 py-1 mb-4">
             OUR VALUES
           </button>
-          <div className="flex flex-col mt-44 text-[#67645E] gap-4">
+          <div className="flex flex-col mt-10 lg:mt-44 text-[#67645E] gap-4">
             <p className="text-[26px] border-t-[1px] border-[#67645E] font-bold mb-2">
               mission
             </p>
@@ -30,7 +30,7 @@ const RestoreSection = () => {
           width={585}
           height={704}
           alt="Hailey Bieber"
-          className="rounded-xl"
+          className="rounded-xl lg:w-[585px] md:w-full"
         />
       </div>
     </div>
