bcdfab68d684805545e8245feecf282ba901f383
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-16 16:26:34 +0600

    Adding holiday section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import HolidaySection from "@/components/HolidaySection";
 import RestoreSection from "@/components/RestoreSection";
 import Slider from "@/components/SliderSection";
 
 export default function Home() {
   return (
     <>
+      <HolidaySection />
       <Slider />
       <RestoreSection />
     </>
---
+++ src/components/HolidaySection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HolidaySection = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="bg-holiday-section-img h-screen rounded-xl flex justify-center items-end py-8">
+        <button className="border h-[35px] border-white text-white px-4 py-1 rounded-full hover:bg-gray-200 font-medium transition">
+          THE HOLIDAY SHOP
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default HolidaySection;
