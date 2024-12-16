021c4af41a8e900b22a505f146e4e74424be5205
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-16 16:30:49 +0600

    Made holiday section responsive

--- src/components/HolidaySection.jsx
+++ src/components/HolidaySection.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const HolidaySection = () => {
   return (
     <div className="container mx-auto">
-      <div className="bg-holiday-section-img h-screen rounded-xl flex justify-center items-end py-8">
-        <button className="border h-[35px] border-white text-white px-4 py-1 rounded-full hover:bg-gray-200 font-medium transition">
+      <div className="bg-holiday-section-img bg-center h-screen rounded-xl flex justify-center items-end py-8">
+        <button className="border h-[35px] border-white text-white px-4 py-1 rounded-full hover:bg-white hover:text-gray-500 font-medium transition">
           THE HOLIDAY SHOP
         </button>
       </div>
