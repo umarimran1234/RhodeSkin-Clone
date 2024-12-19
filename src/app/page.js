e872c2356e93c22b12b8c45583447305c48e6770
UmarArshad-DT <umar@darkentech.com>
2024-12-19 23:08:15 +0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import HolidaySection from "@/components/HolidaySection";
+import ProductCard from "@/components/ProductCard";
 import RestoreSection from "@/components/RestoreSection";
+import SideToSideSection from "@/components/SideToSideSection";
 import Slider from "@/components/SliderSection";
 
 export default function Home() {
