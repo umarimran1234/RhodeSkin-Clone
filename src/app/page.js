26df801dccb314e9c1c56ef22e90ec25b4b10ac6
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-19 16:21:18 +0600

    Added good call section

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import GoodCallSection from "@/components/GoodCallSection";
 import HolidaySection from "@/components/HolidaySection";
 import RestoreSection from "@/components/RestoreSection";
 import Slider from "@/components/SliderSection";
@@ -6,6 +7,7 @@ import TravelSection from "@/components/TravelSection";
 export default function Home() {
   return (
     <>
+      <GoodCallSection />
       <TravelSection />
       <HolidaySection />
       <Slider />
---
+++ src/components/GoodCallSection.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import React from "react";
+
+const GoodCallSection = () => {
+  return (
+    <div className="container mx-auto my-8">
+      {/* Central Div */}
+      <div className="flex md:flex-row-reverse flex-col-reverse">
+        {/* First Child Element */}
+        <Image
+          src={"/GoodCallImg.webp"}
+          alt="Travel Section Image"
+          width={1000}
+          height={1000}
+          className="md:w-1/2 w-full rounded-r-xl"
+        ></Image>
+        {/* Second Child Div */}
+        <div className="md:w-1/2 w-full p-8 bg-[#efefef] rounded-l-xl flex flex-col justify-end">
+          <h3 className="text-[34px] text-[#67645E] md:w-2/3 font-bold">
+            text. treat. tint.
+          </h3>
+          <p className="text-[#67645E] md:w-2/3 font-medium my-4">
+            Take rhode with you everywhere this winter. The Lip Case is
+            compatible with iPhone and your favorite Peptide Lip Treatment or
+            Tint.
+          </p>
+          <button className="border lg:w-1/4 md:w-1/2 w-2/3 h-[35px] border-[#67645E] text-[#67645E] px-4 py-1 rounded-full hover:bg-white hover:text-gray-500 font-medium transition">
+            GOOD CALL
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GoodCallSection;
