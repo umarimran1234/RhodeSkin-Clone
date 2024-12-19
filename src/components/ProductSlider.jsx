b2335ed5ff3f550a904ab3f4366d9b8a13ae1832
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-19 18:25:55 +0600

    Added slider section

--- src/components/ProductSlider.jsx
+++ src/components/ProductSlider.jsx
@@ -14,7 +14,7 @@ const ProductSlider = () => {
       price: "$84.00",
       product: "THE TRAVEL SET",
       reviews: 2,
-      image: "/FirstProduct.webp",
+      image: "/FirstProduct.png",
     },
     {
       title: "prep",
@@ -22,7 +22,7 @@ const ProductSlider = () => {
       price: "$30.00",
       product: "GLAZING MILK",
       reviews: 3437,
-      image: "/SecondProduct.webp",
+      image: "/SecondProduct.png",
     },
     {
       title: "seal",
@@ -30,7 +30,7 @@ const ProductSlider = () => {
       price: "$22.00",
       product: "BARRIER BUTTER",
       reviews: 563,
-      image: "/ThirdProduct.webp",
+      image: "/ThirdProduct.png",
     },
     {
       title: "cleanse",
@@ -38,7 +38,7 @@ const ProductSlider = () => {
       price: "$28.00",
       product: "PINEAPPLE REFRESH",
       reviews: 1325,
-      image: "/FourthProduct.webp",
+      image: "/FourthProduct.png",
     },
     {
       title: "nourish",
@@ -46,7 +46,7 @@ const ProductSlider = () => {
       price: "$18.00",
       product: "peptide lip treatment",
       reviews: 15490,
-      image: "/FifthProduct.webp",
+      image: "/FifthProduct.png",
     },
     {
       title: "blush",
@@ -54,39 +54,39 @@ const ProductSlider = () => {
       price: "$24.00",
       product: "POCKET BLUSH",
       reviews: 3441,
-      image: "/SixthProduct.webp",
+      image: "/SixthProduct.png",
     },
     {
-      title: "glow",
-      subtitle: "Glow enhancing lotion",
-      price: "$28.00",
-      product: "GLOW LOTION",
-      reviews: 950,
-      image: "image7.png",
+      title: "blush + tint",
+      subtitle: "Cozy fall combos",
+      price: "$42.00",
+      product: "the fall duos ",
+      reviews: 329,
+      image: "/SeventhProduct.png",
     },
     {
-      title: "refresh",
-      subtitle: "Refreshing toner",
+      title: "tint",
+      subtitle: "The tinted lip layer",
       price: "$18.00",
-      product: "TONER",
-      reviews: 789,
-      image: "image8.png",
+      product: "peptide lip tint ",
+      reviews: 9491,
+      image: "/EightProduct.png",
     },
     {
-      title: "repair",
-      subtitle: "Repairing night cream",
-      price: "$50.00",
-      product: "NIGHT CREAM",
-      reviews: 1340,
-      image: "image9.png",
+      title: "case",
+      subtitle: "Four limited edition colors",
+      price: "$38.00",
+      product: "fall lip case",
+      reviews: 489,
+      image: "/NineProduct.png",
     },
     {
-      title: "protect",
-      subtitle: "Daily SPF moisturizer",
-      price: "$25.00",
-      product: "SPF MOISTURIZER",
-      reviews: 2000,
-      image: "image10.png",
+      title: "case",
+      subtitle: "Your essentials in one place",
+      price: "$38.00",
+      product: "lip case",
+      reviews: 1363,
+      image: "/TenProduct.png",
     },
   ];
 
@@ -109,25 +109,25 @@ const ProductSlider = () => {
         style={{ transform: `translateX(-${(currentIndex / 3) * 100}%)` }}
       >
         {cards.map((card, index) => (
-          <div key={index} className="min-w-[33.3333%] p-4">
-            <div className="bg-gray-100 text-[#67645E] rounded-lg shadow-lg p-6">
-              <h3 className="text-[4vw] font-bold mb-2">
-                {card.title}
-              </h3>
+          <div key={index} className="md:min-w-[33.3333%] p-4">
+            <div className="bg-gray-100 lg:h-[75vh] md:h-[90vh] h-[95vh] text-[#67645E] rounded-lg shadow-lg p-6">
+              <h3 className="text-[4vw] font-bold mb-2">{card.title}</h3>
               <img
                 src={card.image}
                 alt={card.product}
-                className="w-full h-40 object-cover mb-4"
+                className="w-full h-[40vh] object-cover mb-4"
               />
-              <div className="flex justify-between">
+              <div className="flex lg:flex-row flex-col justify-between">
                 <div>
                   <p className=" flex items-center font-medium gap-1">
-                    {stars.map((star) => (
-                      <IoIosStar key={star.index} />
+                    {stars.map((index) => (
+                      <IoIosStar key={index} />
                     ))}{" "}
                     ({card.reviews})
                   </p>
-                  <p className="text-[17px] uppercase font-semibold">{card.product}</p>
+                  <p className="text-[17px] uppercase font-semibold">
+                    {card.product}
+                  </p>
                   <p className="font-medium">{card.subtitle}</p>
                 </div>
                 <p className="text-lg font-bold mt-4">{card.price}</p>
