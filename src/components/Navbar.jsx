a927340a2cb601a2a72f3122fa63bef3f434dc72
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-20 19:51:33 +0600

    Made navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 const Navbar = () => {
   const [show, setShow] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     let lastScrollY = window.scrollY;
@@ -19,13 +20,54 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav
       className={`${
         show ? "translate-y-9 bg-[#67645E]" : " -translate-y-60 bg-[#67645E] "
-      } bg-transparent fixed w-full top-10 z-10 transition-all duration-300 container mx-auto text-white font-bold`}
+      } bg-transparent fixed w-full top-5 z-10 transition-all duration-300 container mx-auto text-white font-bold`}
     >
-      <ul className="flex md:flex-row flex-col space-y-2 md:space-y-0 md:items-center md:justify-center items-end p-4 lg:space-x-10 md:space-x-7">
+      <div className="flex md:hidden justify-between items-center p-4">
+        <a href="#futures" className="text-5xl lg:px-56">
+          rhode
+        </a>
+        <button
+          className="md:hidden block text-white focus:outline-none"
+          onClick={toggleMenu}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {isMenuOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            )}
+          </svg>
+        </button>
+      </div>
+      <ul
+        className={`${
+          isMenuOpen ? "block bg-gray-50 text-gray-400" : "hidden"
+        } md:flex md:flex-row flex-col space-y-2 md:items-center md:justify-center p-4 lg:space-x-10 md:space-x-7`}
+      >
         <li>
           <a href="#shop">SHOP</a>
         </li>
@@ -35,7 +77,7 @@ const Navbar = () => {
         <li>
           <a href="#futures">FUTURES</a>
         </li>
-        <li className="lg:px-56">
+        <li className="hidden md:block lg:px-56">
           <a href="#futures" className="text-5xl">
             rhode
           </a>
