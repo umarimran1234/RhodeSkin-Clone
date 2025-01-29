bd2eb874e20b64a38c52e551827575f07990b7b8
umarimran1234 <umartkd989@gmail.com>
2025-01-29 23:10:58 +0500

    upate

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,10 +24,27 @@ const quantico = Quantico({
 });
 
 export const metadata = {
-  title: "Zalmar Store",
-  description: "style that speaks your life style",
+  title: "Zalmar - Premium Clothing Brand",
+  description:
+    "Shop high-quality fashion at Zalmar. Explore the latest trends in clothing.",
+  keywords:
+    "zalmar, clothing, fashion, online store, premium wear, buy clothes online in Pakistan, best clothing brand in Pakistan, trendy fashion wear in Pakistan, online clothing store Pakistan, affordable fashion for men & women, buy designer clothes in Pakistan, latest fashion trends 2024, branded clothes for men & women, top online shopping sites for clothing, fashionable dresses for sale, zalmar fashion brand Pakistan, zalmar online clothing store, zalmar premium wear for men & women, zalmar trendy clothes for sale, buy zalmar clothes online in Pakistan",
+  openGraph: {
+    title: "Zalmar - Premium Clothing",
+    description: "Discover the latest fashion trends with Zalmar. Shop now!",
+    url: "https://zalmar.store",
+    siteName: "Zalmar",
+    images: [
+      {
+        url: "https://zalmar.store/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Zalmar Clothing Store",
+      },
+    ],
+    type: "website",
+  },
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
